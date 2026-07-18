dfb8e70681d58d73b74b36d5a3077d5282c8dc5a
Hattori <frannsm97@gmail.com>
2026-07-18 21:37:37 +0100

    chore: improve project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+    expoConfig,
+    {
+        rules: {
+            eqeqeq: ["warn", "always"],
+            "no-var": "error",
+            "prefer-const": "warn",
+            "no-unused-vars": "warn",
+            "consistent-return": "warn",
+            "arrow-body-style": ["warn", "as-needed"],
+            complexity: ["warn", 10]
+        }
+    },
+    {
+        ignores: ["dist/*"]
+    }
 ]);

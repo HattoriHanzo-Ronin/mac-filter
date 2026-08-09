78905f145560bf0d2c12312366501adfa904a8a1
Hattori <frannsm97@gmail.com>
2026-08-09 21:40:10 +0000

    feat(devices): integrate add and edit forms

--- src/app/main-app/add.tsx
+++ src/app/main-app/add.tsx
@@ -1,33 +1,31 @@
-import { View, Text } from "react-native";
-import Formu from "@/src/components/formu";
-import { useState } from "react";
-import { add } from "../../styles";
 import Background from "@/src/components/background";
-import UiUtils from "@/src/utils/ui-utils";
+import { Redirect } from "expo-router";
+import DeviceForm from "@/src/components/device-form";
+import { CreateDeviceRequest } from "@/src/types/devices";
+import { DeviceFormRequest } from "@/src/types/form";
+import { useAppContext } from "@/src/components/app-context-provider";
+import { ApiErrorResponse } from "@/src/types/api-response";
 
 export default function Add() {
-    const onSubmit = () => {
-        setName("");
-        setType("");
-    };
-    const [name, setName] = useState("");
-    const [type, setType] = useState("");
+    const { setDevices, executeApiRequest, isAuthenticated } = useAppContext();
+
+    async function createDevice(request: DeviceFormRequest): Promise<ApiErrorResponse | undefined> {
+        const result = await executeApiRequest((apiUtils) => apiUtils.createDevice(request as CreateDeviceRequest));
+        if (!result.success) {
+            return result;
+        }
+
+        setDevices((devices) => [...devices, result.data]);
+        return undefined;
+    }
+
+    if (!isAuthenticated) {
+        return <Redirect href="/" />;
+    }
+
     return (
         <Background>
-            <View style={add.parent}>
-                <Text style={add.titleForm}>Añadir {UiUtils.makeName(name, type)}</Text>
-                <Formu
-                    onSubmit={onSubmit}
-                    onChangeName={(tx: string) => {
-                        setName(tx);
-                        return tx;
-                    }}
-                    onChangeType={(tx: string) => {
-                        setType(tx);
-                        return tx;
-                    }}
-                />
-            </View>
+            <DeviceForm onSubmit={createDevice} />
         </Background>
     );
 }

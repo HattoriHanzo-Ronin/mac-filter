082bc15fd88b141b2982e1633c9802f114d4bf5a
Hattori <frannsm97@gmail.com>
2026-08-29 08:54:39 +0000

    feat: add user menu to navigation header

--- src/app/tab-nav-screens/_layout.tsx
+++ src/app/tab-nav-screens/_layout.tsx
@@ -1,14 +1,26 @@
-import TabNav from "@/src/components/tab-nav";
+import { withLayoutContext } from "expo-router";
+import { createMaterialTopTabNavigator } from "expo-router/js-top-tabs";
+
+const Tab = withLayoutContext(createMaterialTopTabNavigator().Navigator);
 
 export default function Layout() {
+    const tabs = [
+        { name: "Agregar", path: "add" },
+        { name: "Lista", path: "index" },
+        { name: "Editar", path: "edit" }
+    ];
+
     return (
-        <TabNav
-            tabs={[
-                { name: "Agregar", path: "add" },
-                { name: "Lista", path: "index" },
-                { name: "Editar", path: "edit" }
-            ]}
-            init={"index"}
-        />
+        <Tab
+            initialRouteName="index"
+            screenOptions={{
+                tabBarActiveTintColor: "rgba(13, 134, 9, 0.99)",
+                tabBarInactiveTintColor: "rgba(31, 44, 219, 0.42)"
+            }}
+        >
+            {tabs.map(({ name, path }) => (
+                <Tab.Screen key={path} name={path} options={{ title: name }} />
+            ))}
+        </Tab>
     );
 }
